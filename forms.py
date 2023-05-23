0513f7258b2ec2178a34a337df7dd7c5cf253978
Bradley Butler <bradley@Bradleys-MacBook-Pro.local>
2023-05-23 17:44:27 -0600

    fix requirements.txt

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, widgets, SelectMultipleField
+from wtforms import StringField, PasswordField, widgets, SelectMultipleField
 from wtforms.validators import InputRequired, Optional, Email, Length
 from markupsafe import Markup
 
